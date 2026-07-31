357d8dc134e699d02e33c3d2d7f4d5f86137602a
LLop3zs <dev.miguel.gomes@gmail.com>
2026-07-31 13:23:44 -0300

    Criação do repository PecaRoupa

---
+++ backend/projeto-vestuario/src/main/java/com/projetovestuario/repository/PecaRoupaRepository.java
@@ -0,0 +1,9 @@
+package com.projetovestuario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetovestuario.entities.PecaRoupa;
+
+public interface PecaRoupaRepository extends JpaRepository<PecaRoupa, Long> {
+
+}
